a73e343e81ece09e6f1d3a98a2efea1f711195e9
husterknupp <husterknupp@outlook.com>
2017-01-18 10:58:19 +0500

    field as class/component

--- client/react/board.jsx
+++ client/react/board.jsx
@@ -1,7 +1,7 @@
-class Board extends React.Component {
+// TODO extract module - ?use webpack
+class Field extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {fields: []};
     this.tokenStyle = {
       width: '100px'
       , height: '100px'
@@ -22,22 +22,59 @@ class Board extends React.Component {
 
   render() {
     return (
+      <a style={this.tokenStyle} className={this.tokenClass} role={this.tokenRole}>{this.props.value}</a>
+      );
+  }
+}
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {fields: this.getNulledArray()};
+    this.tokenStyle = {
+      width: '100px'
+      , height: '100px'
+      , display: 'inline-block'
+      , backgroundColor: '#eee'
+      , margin: '10px'
+      , fontSize: '64px'
+    }
+    this.tokenClass = "text-center";
+    this.tokenRole = "button";
+  }
+
+  getNulledArray() {
+    return [
+        null, null, null
+        , null, null, null
+        , null, null, null
+      ];
+  }
+
+  componentDidMount() {
+  }
+
+  componentWillUnmount() {
+  }
+
+  render() {
+    return (
       <table>
         <tbody>
           <tr>
-            <th><a style={this.tokenStyle} className={this.tokenClass} role={this.tokenRole} onClick={this.handleClick}>c</a></th>
-            <th><a style={this.tokenStyle} className={this.tokenClass} role={this.tokenRole}></a></th>
-            <th><a style={this.tokenStyle} className={this.tokenClass} role={this.tokenRole}></a></th>
+            <th><Field value={this.state.fields[0]}/></th>
+            <th><Field value={this.state.fields[1]}/></th>
+            <th><Field value={this.state.fields[2]}/></th>
           </tr>
           <tr>
-            <th><a style={this.tokenStyle} className={this.tokenClass} role={this.tokenRole}></a></th>
-            <th><a style={this.tokenStyle} className={this.tokenClass} role={this.tokenRole}></a></th>
-            <th><a style={this.tokenStyle} className={this.tokenClass} role={this.tokenRole}></a></th>
+            <th><Field value={this.state.fields[3]}/></th>
+            <th><Field value={this.state.fields[4]}/></th>
+            <th><Field value={this.state.fields[5]}/></th>
           </tr>
           <tr>
-            <th><a style={this.tokenStyle} className={this.tokenClass} role={this.tokenRole}></a></th>
-            <th><a style={this.tokenStyle} className={this.tokenClass} role={this.tokenRole}></a></th>
-            <th><a style={this.tokenStyle} className={this.tokenClass} role={this.tokenRole}></a></th>
+            <th><Field value={this.state.fields[6]}/></th>
+            <th><Field value={this.state.fields[7]}/></th>
+            <th><Field value={this.state.fields[8]}/></th>
           </tr>
         </tbody>
       </table>
@@ -48,19 +85,6 @@ class Board extends React.Component {
     console.log('abc');
   }
 }
-
-// ReactDOM.render(
-//   <Clock />,
-//   document.getElementById('root')
-// ); 
-
-// function App() {
-//   return (
-//     <div>
-//       <Board />
-//     </div>
-//   );
-// }
 
 ReactDOM.render(
   <Board />,
